41163cd3a923b4b1b77fdd62516ec2dba70ba1a4
pipox1 <126177325+pipox1@users.noreply.github.com>
2026-02-04 15:51:47 -0700

    Create me.js

---
+++ api/me.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+  
+  if (!token) {
+    return res.status(401).json({ error: 'No token' });
+  }
+  
+  try {
+    const response = await axios.get('https://api.procore.com/rest/v1.0/me', {
+      headers: { 'Authorization': `Bearer ${token}` }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+};
