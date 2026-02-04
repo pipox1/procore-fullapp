35f0a44e454f04f2e59d1b517e2b785d165ecdc9
pipox1 <126177325+pipox1@users.noreply.github.com>
2026-02-04 15:54:18 -0700

    Create punch-list.js

---
+++ api/punch-list.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+  const { project_id, company_id } = req.query;
+  
+  if (!token) return res.status(401).json({ error: 'No token' });
+  
+  try {
+    const response = await axios.get(
+      `https://api.procore.com/rest/v1.0/projects/${project_id}/punch_items`,
+      { headers: { 'Authorization': `Bearer ${token}`, 'Procore-Company-Id': company_id } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+};
