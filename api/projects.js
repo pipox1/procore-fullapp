5c6e226cf6da1ea28e4992b15b0cc0c1c72dfa98
pipox1 <126177325+pipox1@users.noreply.github.com>
2026-02-04 15:52:55 -0700

    Create projects.js

---
+++ api/projects.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+  const { company_id } = req.query;
+  
+  if (!token) return res.status(401).json({ error: 'No token' });
+  
+  try {
+    const response = await axios.get(
+      `https://api.procore.com/rest/v1.0/companies/${company_id}/projects`,
+      { headers: { 'Authorization': `Bearer ${token}` } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+};
